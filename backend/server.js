4f6bf014172aadd9e828470548fe024cb1966853
VIGNESH VASU <vigneshvasu0511@gmail.com>
2024-06-23 15:49:29 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,20 +10,14 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-    allowedHeaders: ["my-custom-header"],
-    credentials: true
+    origin: "*",
+    methods: ["GET", "POST"]
   }
 });
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
-
-// Middleware
-app.use(cors());
-app.use(express.json());
 
 // Connect to MongoDB
 mongoose.connect(MONGO_URI, {
@@ -32,6 +26,9 @@ mongoose.connect(MONGO_URI, {
 })
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err));
+
+// Middleware
+app.use(cors());
 
 // WebSocket communication
 io.on('connection', socket => {
